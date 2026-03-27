64e2143a4ac81a975f830fc863ed26aa9c87f2da
mejba.13 <mejba.13@gmail.com>
2026-03-27 07:52:37 +0600

    feat: add Result pattern for error handling

---
+++ flyone/lib/core/utils/result.dart
@@ -0,0 +1,14 @@
+sealed class Result<T> {
+  const Result();
+}
+
+class Success<T> extends Result<T> {
+  final T data;
+  const Success(this.data);
+}
+
+class Failure<T> extends Result<T> {
+  final String message;
+  final Exception? exception;
+  const Failure(this.message, [this.exception]);
+}
